7333d68bd55f86debe10f2c299b215eda2c7c3f3
Wallacy fernandez <111294600+WallacyFernandez@users.noreply.github.com>
2024-06-12 15:13:56 -0300

    Adaptando projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import { About, Contact, Feedbacks, Hero, Navbar, Works, StarsCanvas } from "./components";
 
 const App = () => {
   return (
@@ -10,11 +10,9 @@ const App = () => {
           <Navbar />
           <Hero />
         </div>
-        <About />
-        <Experience />
-        <Tech />
-        <Works />
+        <About/>
         <Feedbacks />
+        <Works />
         <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
