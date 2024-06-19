3bbe27309950a99c3ce4dfc618cf6accdcfdfd39
Wallacy fernandez <111294600+WallacyFernandez@users.noreply.github.com>
2024-06-18 23:19:11 -0300

    add: 3d material

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -5,7 +5,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Earth = () => {
-  const earth = useGLTF("./planet/scene.gltf");
+  const earth = useGLTF("./planet_earth/scene.gltf");
 
   return (
     <primitive object={earth.scene} scale={0.3} position-y={0} rotation-y={0} />
@@ -30,8 +30,8 @@ const EarthCanvas = () => {
         <OrbitControls
           autoRotate
           enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
+          maxPolarAngle={Math.PI}
+          minPolarAngle={0}
         />
         {/* Add lights to the scene */}
         <hemisphereLight intensity={0.35} groundColor="black" />
