44b8950a65da493fa1aa51813eabb226b74ffa1a
Wallacy fernandez <111294600+WallacyFernandez@users.noreply.github.com>
2024-06-25 16:27:59 -0300

    Add: build

--- .vite/deps/react-tilt.js
+++
@@ -1,9 +0,0 @@
-import {
-  Tilt
-} from "./chunk-GM2KXUWH.js";
-import "./chunk-2PA4WPI3.js";
-import "./chunk-ROME4SDB.js";
-export {
-  Tilt
-};
-//# sourceMappingURL=react-tilt.js.map
--- src/components/canvas/index.js
+++
@@ -1,5 +0,0 @@
-import EarthCanvas from "./Earth";
-import StarsCanvas from "./Stars";
-import AstronautCanvas from "./astronalt";
-
-export { EarthCanvas, StarsCanvas, AstronautCanvas};
