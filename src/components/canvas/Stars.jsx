1bbed2983d3aa11563b588d5ab03936bad3fd8a5
Wallacy fernandez <111294600+WallacyFernandez@users.noreply.github.com>
2024-06-25 18:14:56 -0300

    add: new stars

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -5,11 +5,11 @@ import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = (props) => {
   const ref = useRef();
-  const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
+  const [sphere] = useState(() => random.inSphere(new Float32Array(3000), { radius: 1.2 }));
 
   useFrame((state, delta) => {
-    ref.current.rotation.x -= delta / 10;
-    ref.current.rotation.y -= delta / 15;
+    ref.current.rotation.x -= delta / 20;
+    ref.current.rotation.y -= delta / 30;
   });
 
   return (
